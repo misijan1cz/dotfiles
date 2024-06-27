45cbb85aa85a71a18eeaec93fb10f5e02d5520a5
Michal Janata <mis.janata@gmail.com>
2024-06-27 07:36:21 +0200

    27.06.2024

--- set_colorscheme.sh
+++ set_colorscheme.sh
@@ -7,14 +7,14 @@ COL_SCHEME_NAMES=(COL_TEXT_LIGHT	COL_TEXT_DARK	COL_WARN_LIGHT	COL_WARN_DARK	COL_
 COL_SCHEME_COLOR=("cdd6f4"			"4974a5"		"f4cdd6"		"ff4444"		"000000"	"00000050"		"0, 0, 0, 0.5")
 
 # Swaylock
-cp "$maindir/dotswaylock/config.template" "/home/$username/.swaylock/config"
+cp "$maindir/templates/config.swaylock-template" "/home/$username/.swaylock/config"
 for (( i=0; i<${#COL_SCHEME_NAMES[@]}; i++ ))
 do
 	sed -i "s/(${COL_SCHEME_NAMES[$i]})/${COL_SCHEME_COLOR[$i]}/g" "/home/$username/.swaylock/config"
 done
 
 # Waybar
-cp "$maindir/dotconfig/waybar/style.css.template" "/home/$username/.config/waybar/style.css"
+cp "$maindir/templates/style.css.waybar-template" "/home/$username/.config/waybar/style.css"
 for (( i=0; i<${#COL_SCHEME_NAMES[@]}; i++ ))
 do
 	sed -i "s/(${COL_SCHEME_NAMES[$i]})/${COL_SCHEME_COLOR[$i]}/g" "/home/$username/.config/waybar/style.css"
